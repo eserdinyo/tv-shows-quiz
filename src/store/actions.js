17ec0328006baafbba422c7267fe9b86024ed7ca
eserdinyo <muhammeteser29@gmail.com>
2019-07-25 11:41:44 +0300

    fix actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,61 +2,46 @@ import { seriesRef } from "@/firebase";
 import firebase from 'firebase';
 
 export default {
-    getAllSeries({ commit }) {
-        return new Promise((resolve, reject) => {
-            // GET DATA FROM FIRESTORE
-            seriesRef.get().then(snapshot => {
-                const data = snapshot.docs.map(res => {
-                    const data = res.data();
-                    data.id = res.id;
-                    return data;
-                });
+	getAllSeries({ commit }) {
+		// GET DATA FROM FIRESTORE
+		return seriesRef.get().then(snapshot => {
+			const series = snapshot.docs.map(res => {
+				const data = res.data();
+				data.id = res.id;
+				return data;
+			});
 
-                commit('SET_SERIES', data)
-                resolve(data);
-            })
-        })
-    },
-    getOneShow({ commit }, id) {
-        return new Promise((resolve, reject) => {
-            seriesRef
-                .doc(id)
-                .get().then(res => {
-                    commit('SET_ONE_SHOW', res.data());
-                    resolve(res);
-                })
-
-        })
-
-    },
-    addShow({ _ }, payload) {
-        // ADD DATA TO FIRESTORE
-        return new Promise((resolve, reject) => {
-            seriesRef
-                .add({
-                    name: payload.name,
-                    coverImage: payload.coverImage,
-                    bgImage: payload.bgImage,
-                    music: payload.music
-                }).then(res => {
-                    resolve(res)
-                })
-        })
-    },
-    addCharacter({ _ }, payload) {
-        return new Promise((resolve, reject) => {
-            const data = {
-                name: payload.character.name,
-                imageUrl: payload.character.imageUrl,
-                answers: payload.character.answers
-            }
-            seriesRef
-                .doc(payload.id).update({
-                    characters: firebase.firestore.FieldValue.arrayUnion(data)
-                }).then(res => {
-                    if (!res)
-                        resolve(200)
-                })
-        })
-    }
+			commit('SET_SERIES', series)
+			return series;
+		})
+	},
+	getOneShow({ commit }, id) {
+		return seriesRef
+			.doc(id)
+			.get().then(res => {
+				commit('SET_ONE_SHOW', res.data());
+				return res;
+			})
+	},
+	addShow(_, payload) {
+		// ADD DATA TO FIRESTORE
+		return seriesRef
+			.add({
+				name: payload.name,
+				coverImage: payload.coverImage,
+				bgImage: payload.bgImage,
+				music: payload.music
+			}).then(res => res.data)
+	},
+	addCharacter(_, payload) {
+		const data = {
+			name: payload.character.name,
+			imageUrl: payload.character.imageUrl,
+			answers: payload.character.answers
+		}
+		return seriesRef
+			.doc(payload.id).update({
+				characters: firebase.firestore.FieldValue.arrayUnion(data)
+			}).then(res => !res ? res : "")
+	}
 }
